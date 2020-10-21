976f6fb988500b00685fa4535a82f5a630e1c057
ovarr <otto.varrela@gofore.com>
2020-10-22 01:33:24 +0300

    extension actions on page reload

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-chrome.storage.onChanged.addListener(changes => {
-  if (changes.isExtensionActive) {
-    rmSellBtn(changes.isExtensionActive.newValue)
+chrome.storage.onChanged.addListener((changes) => {
+  if (changes) {
+    chrome.storage.sync.get('extensionActive', storage => {
+      rmSellBtn(storage.extensionActive)
+    })
   }
 })
 
 const rmSellBtn = (state) => {
-  chrome.runtime.sendMessage({"active": state})
+  chrome.runtime.sendMessage({state: state})
   const sellBtn = document.querySelectorAll('a[href*="/toimeksianto/myy"]');
   state ? sellBtn.forEach((a) => a.style.visibility = 'hidden') : sellBtn.forEach((a) => a.style.visibility = 'visible')
 }
